a7b545b552cfcb7ba45da36626a9176806f40edf
Botond Barabas <bboti86@gmail.com>
2019-12-31 00:15:49 +0100

    added auth

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -39,12 +39,12 @@ def register():
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
-  if request.method == 'POST'
+  if request.method == 'POST':
     username = request.form['username']
     password = request.form['password']
     db = get_db()
     error = None
-    user = db.execute('SELECT * FROM user WHERE username = ?', (username,).fetchone()
+    user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone()
 
     if user is None:
       error = 'Incorrect username.'
