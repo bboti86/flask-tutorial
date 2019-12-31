14cb4da7a2884461e8ebe82138a3579465d3b59b
Botond Barabas <bboti86@gmail.com>
2019-12-31 01:15:43 +0100

    ready for packaging"

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -67,7 +67,7 @@ def load_logged_in_user():
   if user_id is None:
     g.user = None
   else:
-    g.user = get_db().execute('SELECT * FROM user WHERE id = ?', (user_id,)).fethcone()
+    g.user = get_db().execute('SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
 
 
 @bp.route('/logout')
